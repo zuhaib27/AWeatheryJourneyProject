5e85620b29fc39b2832cb3131ccac6967102ba84
Joshua Teichroeb <josh.teichro@gmail.com>
2020-02-22 20:38:39 -0500

    Implemented simple settings.

--- ProjectWeather/Assets/UI/OptionsMenu.cs
+++ ProjectWeather/Assets/UI/OptionsMenu.cs
@@ -1,11 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Assets.Player.Scripts;
 
 public class OptionsMenu : MonoBehaviour
 {
-    public void SetInvertCamera()
+    public GameObject player;
+    private MyPlayer _player;
+
+    private void Start()
     {
-        Debug.Log("Invert Camera");
+        _player = player.GetComponent<MyPlayer>();
+    }
+
+    public void SetInvertCameraY(bool invert)
+    {
+        PlayerSettings settings = _player.Settings;
+        settings.invertCameraY = invert;
+        _player.Settings = settings;
     }
 }
