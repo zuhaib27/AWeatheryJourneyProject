961dd8463f0217b354801875bcaa2774081010b0
Zuhaib <zuhaib.k.2@hotmail.com>
2020-03-05 00:38:09 -0500

    made simple collectible, but does not count amount collected

---
+++ ProjectWeather/Assets/Scripts/collectiblePickUp.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class collectiblePickUp : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, 90 * Time.deltaTime, 0);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("player collected");
+            Destroy(gameObject);
+        }
+    }
+}
