bfd69980e14c9beffca12cc11a0a88034a6e364c
Eric K <eric.koehli@mail.utoronto.ca>
2020-03-08 19:02:59 -0700

    Added CheckpointManager class to manage checkpoints

---
+++ ProjectWeather/Assets/Scripts/Managers/CheckpointManager.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using Assets.Player.Scripts;
+using UnityEngine;
+
+public class CheckpointManager : MonoBehaviour
+{
+    public static CheckpointManager Instance { get; private set; }
+
+
+    // Private variables
+    private Vector3 _currentCheckpoint;
+    private Animator _animator;
+    private MyCharacterController _cc;
+
+    private void Awake()
+    {
+        #region singleton
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else if (Instance != this)
+        {
+            //Destroy(gameObject);
+        }
+        #endregion
+
+        _animator = GetComponent<Animator>();
+        _cc = FindObjectOfType<MyCharacterController>();
+    }
+
+    private void Start()
+    {
+        // Initialize Checkpoint0 to the level's spawn point
+        _currentCheckpoint = _cc.GetCharacterPosition();
+        _currentCheckpoint.y += 0.5f;
+    }
+
+    public void SetCheckpoint(Vector3 pos)
+    {
+        _currentCheckpoint = pos;
+        _currentCheckpoint.y += 0.5f;
+    }
+
+    public void LoadCheckpoint()
+    {
+        _animator.ResetTrigger("FadeOut");
+        _animator.ResetTrigger("FadeIn");
+        _animator.SetTrigger("FadeOut");
+        StartCoroutine(FindObjectOfType<MusicPlayer>().FadeOutSong(1));
+        _cc.Motor.SetPosition(_currentCheckpoint);
+    }
+
+    public void OnFadeOutComplete()
+    {
+        _animator.SetTrigger("FadeIn");
+    }
+}
