1fdc166f87de344c351aea44e95de9fecfc05b2f
Joshua Teichroeb <josh.teichro@gmail.com>
2020-03-18 17:01:45 -0400

    Added fill bucket sound

--- ProjectWeather/Assets/Scripts/Level00/BucketAndRope.cs
+++ ProjectWeather/Assets/Scripts/Level00/BucketAndRope.cs
@@ -2,10 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BucketAndRope : MonoBehaviour
+public class BucketAndRope : Interactible
 {
     Animator _animator;
     RisingWater _risingWater;
+
+    public AudioSource fillSound;
 
     private void Awake()
     {
@@ -18,5 +20,15 @@ public class BucketAndRope : MonoBehaviour
         //_animator.SetVector("", )
         _animator.enabled = _risingWater.DidWaterLevelChange();
         Debug.Log("Did water level change: " + _risingWater.DidWaterLevelChange());
+    }
+
+    public override void OnRainDown(AbilityEvent e)
+    {
+        fillSound.Play();
+    }
+
+    public override void OnRainUp(AbilityEvent e)
+    {
+        fillSound.Stop();
     }
 }
