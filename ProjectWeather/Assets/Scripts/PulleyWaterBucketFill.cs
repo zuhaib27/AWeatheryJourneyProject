a8e838e6e16213e547afdd35e745a0e407f446c0
Zuhaib <zuhaib.k.2@hotmail.com>
2020-03-19 13:25:07 -0400

    added some models for pulley system, very basic

--- ProjectWeather/Assets/Scripts/PulleyWaterBucketFill.cs
+++ ProjectWeather/Assets/Scripts/PulleyWaterBucketFill.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 
 public class PulleyWaterBucketFill : Interactible
 {
-
+    public GameObject water;
+    public float verticalSpeed = 0.5f;
+    public Transform maxWaterLevel;
     // Start is called before the first frame update
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
@@ -19,6 +21,11 @@ public class PulleyWaterBucketFill : Interactible
     public override void OnRain(AbilityEvent e)
     {
         this.transform.parent.GetComponent<PulleySystem>().enableRain = true;
+        if(water.transform.position.y < maxWaterLevel.position.y)
+        {
+            water.transform.position += Vector3.up * verticalSpeed * Time.deltaTime;
+        }
+        
     }
 
     public override void OnRainUp(AbilityEvent e)
